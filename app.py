7a777d0a8175369dc74d965d43ac11224fc6e0e5
raoufk225-blip <raoufk225@gmail.com>
2026-02-17 18:46:47 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -217,7 +217,8 @@ def home():
 
 def run_api():
     print("🌐 بدء تشغيل API...")
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    port = int(os.environ.get("PORT", 8000))
+app.run(host="0.0.0.0", port=port)
 
 def AuTo_ResTartinG():
     time.sleep(6 * 60 * 60)
